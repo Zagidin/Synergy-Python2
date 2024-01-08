1ccd142e6a05e0aa736c9a1c33082581dc6f7967
Zagidin <prap0235@gmail.com>
2024-01-08 23:06:40 +0300

    Zagidin

--- DZ_9/Zmeika.py
+++ DZ_9/Zmeika.py
@@ -2,14 +2,19 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((500, 400))
-pygame.display.set_caption("ZmeiKaZag")
+window = pygame.display.set_mode((700, 500))
+pygame.display.set_caption("ZmeiKa")
 
 icon_game = pygame.image.load('DZ_9/icon/icon.png').convert_alpha()
 pygame.display.set_icon(icon_game)
 
 running = True
 while running:
+
+
+
+    pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
