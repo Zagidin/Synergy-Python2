ec55a06078c62a45e40aed1b304ddc1dd4a8ec8d
Zagidin <prap0235@gmail.com>
2024-01-08 23:36:39 +0300

    Zagidin

--- DZ_9/Zmeika.py
+++ DZ_9/Zmeika.py
@@ -8,10 +8,16 @@ pygame.display.set_caption("ZmeiKa")
 icon_game = pygame.image.load('DZ_9/icon/icon.png').convert_alpha()
 pygame.display.set_icon(icon_game)
 
+img_fon = pygame.image.load('DZ_9/img_fon/fon.png').convert_alpha()
+
+apple = pygame.image.load('DZ_9/images/aple.png').convert_alpha()
+
 running = True
 while running:
 
+    window.blit(img_fon, (0, 0))
 
+    window.blit(apple, (330, 200))
 
     pygame.display.update()
 
